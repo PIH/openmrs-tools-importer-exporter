8e24eeaaaf310f941f36d7185309be861ba0b61e
mogoodrich <mgoodrich@pih.org>
2025-05-07 11:44:21 -0400

    remove console and require from "verify" script

--- src/verify.js
+++ src/verify.js
@@ -1,16 +1,19 @@
-import { loadPatientUuidsFromFile } from "./services/fileService";
-import { exportPatient } from "./services/exporterService";
+import path from "path";
 
-const config = require('../utils/config');
+import {loadPatientUuidsFromDir, loadPatientUuidsFromFile} from "./services/fileService.js";
+import { exportPatient } from "./services/exporterService.js";
+import logger from './utils/logger.js';
+import config from './utils/config.js';
 
 const SUCCESS_DIR = path.join(config.TARGET_DIR, 'successful');
 
-const uuids = loadPatientUuidsFromFile(SUCCESS_DIR);
+const uuids = loadPatientUuidsFromDir(SUCCESS_DIR);
 
 for (const uuid of uuids) {
   try {
     const patientRecord = await exportPatient(uuid);
+    logger.info(`Patient ${uuid} verified successfully`);
   } catch (error) {
-    console.error("Failed verify patient ${uuid}:", error);
+    logger.error("Failed verify patient ${uuid}:", error);
   }
 }
